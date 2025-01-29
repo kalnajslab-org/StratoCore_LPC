a47ee0365ae1535915079bec4d71a689ed94f8cb
MisterMartin <1703358+MisterMartin@users.noreply.github.com>
2025-01-29 06:15:54 -0700

    Put the hex files in a hex/ directory
    It became obvious that it would be a mess to have them in the top project directory.

--- hex_save.py
+++ hex_save.py
@@ -18,9 +18,12 @@ def copy_hex_file(source, target, env):
     build_dir = env.subst("$BUILD_DIR")
     build_env = env.subst("$PIOENV")
     hex_file = os.path.join(build_dir, "firmware.hex")
-    dest_dir = env.subst("$PROJECT_DIR")
+    dest_dir = os.path.join(env.subst("$PROJECT_DIR"), "hex")
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
     build_date = datetime.datetime.now(datetime.timezone.utc).strftime("%Y%m%d-%H%M%S")
     dest_file = os.path.join(dest_dir, f"{build_env}-{build_date}-{git_commit}.hex")
+
     if os.path.exists(hex_file):
         shutil.copy(hex_file, dest_file)
         print(f"Copied {hex_file} to {dest_file}")
